c2172c1e2762580de4a7e67a78d5eac781576a8f
Hasitha <hasithadx99@gmail.com>
2026-08-18 00:16:45 +0530

    Add AdminDashboardController for dashboard route

---
+++ vaxtrack/src/main/java/com/moh/vaxtrack/controller/AdminDashboardController.java
@@ -0,0 +1,13 @@
+package com.moh.vaxtrack.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminDashboardController {
+
+    @GetMapping("/admin/dashboard")
+    public String showDashboard() {
+        return "admin/dashboard";
+    }
+}
