6de87597cae7aa8ba479beb58c163efa4af06e88
Hasitha <hasithadx99@gmail.com>
2026-08-21 19:54:54 +0530

    Add CustomUserDetail security file

---
+++ vaxtrack/src/main/java/com/moh/vaxtrack/security/CustomUserDetails.java
@@ -0,0 +1,59 @@
+package com.moh.vaxtrack.security;
+
+import com.moh.vaxtrack.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getStatus().name().equals("ACTIVE");
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
