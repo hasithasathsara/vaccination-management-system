116cd1201ce55895edb9b4beba533974b17e9d79
Hasitha <hasithadx99@gmail.com>
2026-08-29 15:24:52 +0530

    Add Hospital Status

--- vaxtrack/src/main/java/com/moh/vaxtrack/entity/HospitalStatus.java
+++ vaxtrack/src/main/java/com/moh/vaxtrack/entity/HospitalStatus.java
@@ -1,7 +1,6 @@
 package com.moh.vaxtrack.entity;
 
-// An ENUM is a fixed list of allowed values.
-// A hospital's status can ONLY ever be one of these two things - nothing else.
+
 public enum HospitalStatus {
     ACTIVE,
     INACTIVE
