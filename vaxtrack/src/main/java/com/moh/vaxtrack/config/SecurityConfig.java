08c39e919c4c3dbb55e594d4ce2a6334b03c0d16
Hasitha <hasithadx99@gmail.com>
2026-09-15 07:19:34 +0530

    Update securityconfig

--- vaxtrack/src/main/java/com/moh/vaxtrack/config/SecurityConfig.java
+++ vaxtrack/src/main/java/com/moh/vaxtrack/config/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig {
         return http.build();
     }
 
-   
+
     @Bean
     @Order(2)
     public SecurityFilterChain staffSecurityFilterChain(HttpSecurity http) throws Exception {
